425ad8df0ed45068d039857622e77d8f7bf2fa0a
David Amrani <david.elamrani@telefonica.com>
2023-09-06 23:11:06 +0200

    🍺 Astro 3  - API: Aleatorio / Todos  - Components: Header

--- pages/api/proverbs.js
+++
@@ -1,7 +0,0 @@
-import readJsonFile from '../../utils/reader';
-
-export default function handler(req, res) {
-  readJsonFile('refranes.json')
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(500).json(err));
-}
