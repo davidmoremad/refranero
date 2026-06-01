73a0c848b3bb2f726e566eba67b0e3f8a0b2461e
David Amrani <davidmorenomad@gmail.com>
2026-06-01 23:53:07 +0200

    chore: update pnpm workspace configuration to allow builds for esbuild and sharp
    fix: add reference to Astro types in env.d.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://refranero-espanol.vercel.app/',
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel()
 })
